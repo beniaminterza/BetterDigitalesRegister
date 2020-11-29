0eb87b578b2647ae326b6857aedef38eb95fefd1
Beniamin Terza <beniaminterza@gmail.com>
2020-11-29 11:54:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# What is it?
+
+It's a Chrome extension that allows you to see the average of the marks in the Digitales Register.
+
+# Installation
+
+1. Go to <chrome://extensions/>
+2. Enable the Developer mode on the top left
+3. Click on Load unpacked and choose the directory of the extension
+4. If the extension is not enabled enable it
+5. The extension should now work :)
---
+++ content.js
@@ -0,0 +1,128 @@
+let boolean = true;
+let tryes = 0;
+
+function start() {
+    let url = window.location.href.toString();
+    console.log(url);
+    if (url.includes("subjects")) {
+        console.log("joooooooooo");
+        if (boolean) {
+            console.log("probirma");
+            if(tryes < 6){
+                boolean = false;
+                tryes = 0;
+                addAverageToPage();
+            } else {
+                tryes +=1;
+            }
+        }
+    } else {
+        boolean = true;
+    }
+}
+
+function addAverageToPage() {
+    let subjects = document.getElementsByClassName("student-subject-item");
+    if (subjects.length > 0){
+        boolean = false;
+        tryes = 0;
+    }
+
+    let counter = 0;
+    let sumAllMarks = 0;
+    for (let i = 0; i < subjects.length; i++) {
+        let marks = subjects[i].getElementsByClassName(
+            "grade with-tooltip ng-binding"
+        );
+
+        let sum = 0;
+        let sumDivide = 0;
+        for (let j = 0; j < marks.length; j++) {
+            let percentage = getPercentege(marks[j].title);
+            let mark = getMarkValue(marks[j].innerHTML);
+
+            sum += (mark * percentage) / 100;
+            sumDivide += percentage / 100;
+
+        }
+
+        if (marks.length > 0 && sumDivide > 0) {
+            const average = sum / sumDivide;
+            let averageFixedLength = average;
+            if (average.toString().length > 5)
+                averageFixedLength = averageFixedLength.toFixed(3);
+
+            let valueLeft = subjects[i].getElementsByClassName(
+                "absences ng-binding"
+            )[0];
+            valueLeft.innerHTML = averageFixedLength;
+            valueLeft.title = "Notendurchschnitt";
+            valueLeft.setAttribute("popover-trigger", "none");
+            valueLeft.setAttribute("uib-popover", "Anzahl Fehlstunden");
+
+            valueLeft.style.color = "limegreen";
+            valueLeft.style.fontWeight = "bold";
+
+            sumAllMarks += average;
+            counter++;
+            
+        }
+    }
+    let averageMarks = sumAllMarks / counter;
+    addAverageAllSubjects(averageMarks);
+}
+
+
+setInterval(start, 500);
+
+function getPercentege(string) {
+    for (let i = string.length - 1; i >= 0; i--) {
+        if (string[i] === ">") {
+            return parseInt(string.substring(i + 1, string.length - 1));
+        }
+    }
+}
+
+function getMarkValue(mark) {
+    let number = parseInt(mark);
+    if (mark.length > 1) {
+        if (mark[1] === "/") number += 0.5;
+        else if (mark[mark.length - 1] === "-") number -= 0.25;
+        else if (mark[mark.length - 1] === "+") number += 0.25;
+    }
+    return number;
+}
+
+function addAverageAllSubjects(average){
+    let div = document.createElement("div");
+    div.className = "student-subject-item";
+
+    let parent = document.getElementsByClassName("ng-scope ng-isolate-scope");
+
+    let title = document.createElement("span");
+    title.className = "student-name ng-binding";
+    title.innerHTML = "Notendurchschnitt";
+    title.style.color = "black";
+    title.style.fontWeight = "bold";
+
+    let span = document.createElement("span");
+    span.className = "grades";
+
+
+    let spanMark = document.createElement("span");
+    spanMark.className = "grade with-tooltip ng-binding";
+    spanMark.innerHTML = average;
+    spanMark.style.color = "#6500e0";
+    spanMark.style.fontWeight = "bold";
+
+    span.appendChild(spanMark);
+
+    div.appendChild(title);
+    div.appendChild(span);
+
+    div.style.backgroundColor = "#d2fff0";
+    div.style.fontWeight = "bold";
+    div.style.color = "#f23e02"
+
+    parent[parent.length-2].appendChild(div);
+}
