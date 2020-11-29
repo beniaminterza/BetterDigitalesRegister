e173c294a5f29aad537cb0eea33af5e93030e30a
Beniamin Terza <beniaminterza@gmail.com>
2020-11-29 13:24:47 +0100

    bug fixed

--- content.js
+++ content.js
@@ -1,20 +1,10 @@
 let boolean = true;
-let tryes = 0;
 
 function start() {
     let url = window.location.href.toString();
-    console.log(url);
     if (url.includes("subjects")) {
-        console.log("joooooooooo");
         if (boolean) {
-            console.log("probirma");
-            if(tryes < 6){
-                boolean = false;
-                tryes = 0;
-                addAverageToPage();
-            } else {
-                tryes +=1;
-            }
+            addAverageToPage();
         }
     } else {
         boolean = true;
@@ -23,53 +13,53 @@ function start() {
 
 function addAverageToPage() {
     let subjects = document.getElementsByClassName("student-subject-item");
-    if (subjects.length > 0){
-        boolean = false;
-        tryes = 0;
-    }
 
-    let counter = 0;
-    let sumAllMarks = 0;
-    for (let i = 0; i < subjects.length; i++) {
-        let marks = subjects[i].getElementsByClassName(
-            "grade with-tooltip ng-binding"
-        );
-
-        let sum = 0;
-        let sumDivide = 0;
-        for (let j = 0; j < marks.length; j++) {
-            let percentage = getPercentege(marks[j].title);
-            let mark = getMarkValue(marks[j].innerHTML);
-
-            sum += (mark * percentage) / 100;
-            sumDivide += percentage / 100;
-
+    if(subjects[subjects.length-1].getElementsByClassName("student-name ng-binding")[0].innerHTML !== "Notendurchschnitt"){
+        let counter = 0;
+        let sumAllMarks = 0;
+        for (let i = 0; i < subjects.length; i++) {
+            let marks = subjects[i].getElementsByClassName(
+                "grade with-tooltip ng-binding"
+            );
+    
+            let sum = 0;
+            let sumDivide = 0;
+            for (let j = 0; j < marks.length; j++) {
+                let percentage = getPercentege(marks[j].title);
+                let mark = getMarkValue(marks[j].innerHTML);
+    
+                sum += (mark * percentage) / 100;
+                sumDivide += percentage / 100;
+    
+            }
+    
+            if (marks.length > 0 && sumDivide > 0) {
+                const average = sum / sumDivide;
+                let averageFixedLength = average;
+                if (average.toString().length > 5)
+                    averageFixedLength = averageFixedLength.toFixed(3);
+    
+                let valueLeft = subjects[i].getElementsByClassName(
+                    "absences ng-binding"
+                )[0];
+                valueLeft.innerHTML = averageFixedLength;
+                valueLeft.title = "Notendurchschnitt";
+                valueLeft.setAttribute("popover-trigger", "none");
+                valueLeft.setAttribute("uib-popover", "Anzahl Fehlstunden");
+    
+                valueLeft.style.color = "limegreen";
+                valueLeft.style.fontWeight = "bold";
+    
+                sumAllMarks += average;
+                counter++;
+                
+            }
         }
-
-        if (marks.length > 0 && sumDivide > 0) {
-            const average = sum / sumDivide;
-            let averageFixedLength = average;
-            if (average.toString().length > 5)
-                averageFixedLength = averageFixedLength.toFixed(3);
-
-            let valueLeft = subjects[i].getElementsByClassName(
-                "absences ng-binding"
-            )[0];
-            valueLeft.innerHTML = averageFixedLength;
-            valueLeft.title = "Notendurchschnitt";
-            valueLeft.setAttribute("popover-trigger", "none");
-            valueLeft.setAttribute("uib-popover", "Anzahl Fehlstunden");
-
-            valueLeft.style.color = "limegreen";
-            valueLeft.style.fontWeight = "bold";
-
-            sumAllMarks += average;
-            counter++;
-            
+        if(subjects.length > 0){
+            let averageMarks = sumAllMarks / counter;
+            addAverageAllSubjects(averageMarks);
         }
-    }
-    let averageMarks = sumAllMarks / counter;
-    addAverageAllSubjects(averageMarks);
+    }    
 }
 
 
